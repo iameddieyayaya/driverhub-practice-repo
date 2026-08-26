b4e7489e0d10c23f926e079d521012017ab89ed9
Eduardo <eddieegomez4@gmail.com>
2026-08-26 12:28:15 -0700

    graphql schema practice

---
+++ practice/graphql-node/src/schema.ts
@@ -0,0 +1,17 @@
+/**
+ * TODO(PRACTICE): Replace this placeholder with the Vehicle API schema.
+ *
+ * Required operations:
+ * - Query.vehicles(page, pageSize)
+ * - Query.vehicle(id)
+ * - Mutation.createVehicle(input)
+ * - Mutation.updateVehicle(id, input)
+ * - Mutation.deleteVehicle(id)
+ *
+ * Add Vehicle, VehiclePage, PageInfo/input types as required by the tests.
+ */
+export const typeDefs = /* GraphQL */ `
+
+
+`;
+
